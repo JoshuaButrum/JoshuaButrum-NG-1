6baa79b2bda36dc177bb1caca85643a88d597d8f
Joshua Butrum <jbutrum1s@capetigers.com>
2024-10-04 13:15:08 -0500

    Updated app.component.html, app.component.ts, and app.module.ts

--- NG-1-Tutorial/src/app/app.component.ts
+++ NG-1-Tutorial/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'NG-1-Tutorial';
+    name = 'Josh';
 }
